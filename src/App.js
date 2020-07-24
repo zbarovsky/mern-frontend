7ec4011127736abeb4bc1469c15dee0008bec772
zbarovsky <zachary.barovsky@gmail.com>
2020-07-24 11:01:17 -0700

    added initial navbar and component scaffolding

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Signup from './components/Signup'
 import Login from './components/Login'
 import About from './components/About'
 import Welcome from './components/Welcome'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="react-router-logic">
-      <Switch>
-        <Route path='/signup' component={ Signup } />
-        <Route path='/login' component={Login} />
-        <Route path='/about' component={About} />
-        <Route path='/' component={Welcome} />
-      </Switch>
-      
+    <div>
+      <Navbar />
+      <div className="react-router-logic">
+        <Switch>
+          <Route path='/signup' component={ Signup } />
+          <Route path='/login' component={Login} />
+          <Route path='/about' component={About} />
+          <Route path='/' component={Welcome} />
+        </Switch>
+      </div>
     </div>
   );
 }
